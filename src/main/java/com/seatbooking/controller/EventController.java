c0276355f97cdfca403dcca374bf17db777eeffd
Ojas Mehta <63046043+ojasmehta28@users.noreply.github.com>
2026-05-17 16:07:14 +0530

    Implemented EventService for business logic centralization

--- src/main/java/com/seatbooking/controller/EventController.java
+++ src/main/java/com/seatbooking/controller/EventController.java
@@ -7,28 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@RestController
+@RestController //Indicates that this class is a REST controller, handling HTTP requests and returning responses in JSON format
 @RequestMapping("/events")
-public class EventController {
+public class EventController { //Controller for handling event related requests
 
     @Autowired
     private EventService eventService;
 
     //Create new Event
     @PostMapping
-    public Event createEvent(@RequestBody Event event){
+    public Event createEvent(@RequestBody Event event){ //Endpoint to create a new event, accepting event details in the request body
 
         return eventService.createEvent(event);
     }
 
     //Fetch all events
     @GetMapping
-    public List<Event> getAllEvents(){
+    public List<Event> getAllEvents(){ //Endpoint to fetch all events
 
         return eventService.getAllEvents();
     }
 
-    
+
 
 
 }
