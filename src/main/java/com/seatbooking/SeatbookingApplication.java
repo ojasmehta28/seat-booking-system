6483689b5ab7227eb1618d0f66114dc2b197822d
Ojas Mehta <63046043+ojasmehta28@users.noreply.github.com>
2026-06-12 00:27:47 +0530

    Enable scheduling support for automatic seat lock release

--- src/main/java/com/seatbooking/SeatbookingApplication.java
+++ src/main/java/com/seatbooking/SeatbookingApplication.java
@@ -2,8 +2,10 @@ package com.seatbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // Enable scheduling for tasks like releasing expired seat locks
 public class SeatbookingApplication {
 
 	public static void main(String[] args) {
